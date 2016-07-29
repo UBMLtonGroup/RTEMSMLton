bcd14cb00139971af2b0b3b46f582790ac81dcf3
jeff murphy <jcmurphy@jeffmurphy.org>
2016-07-29 11:52:09 -0400

    gmp deps

--- X86/compile-rtems.sh
+++ X86/compile-rtems.sh
@@ -1,5 +1,5 @@
 RTEMS="/home/vagrant/development/rtems/4.10/i386-rtems4.10/pc386/lib"
-GMP="/mnt/work/RTEMS/gmp-install"
+GMP="/usr/local"
 
 PATH=${PATH}:/home/vagrant/development/rtems/4.10/bin
 
@@ -8,7 +8,7 @@ cd runtime && make clean && cd ..
 
 
 make TARGET=i386-rtems4.10 TARGET_OS=rtems TARGET_ARCH=X86 \
-    CFLAGS="-pipe -B$RTEMS  \
+    CFLAGS="-pipe -I${GMP}/include -L${GMP}/lib -B$RTEMS  \
     -specs bsp_specs -qrtems -g -Wall -msoft-float \
     -Wl,-Ttext,0x00100000 -mtune=i386 -DNO_FENV_H" \
     COMPILE_FAST=yes dirs runtime
